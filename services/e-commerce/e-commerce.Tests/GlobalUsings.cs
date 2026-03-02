99bbf7ecad596c30e70d115865e13942e7a63f39
jaloliddin9917 <jaloliddin9917@gmail.com>
2026-03-02 17:21:30 +0500

    package reference  issue was fixed

--- services/e-commerce/e-commerce.Tests/GlobalUsings.cs
+++ services/e-commerce/e-commerce.Tests/GlobalUsings.cs
@@ -2,7 +2,6 @@
 global using e_commerce.Domain.ValueObjects;
 global using e_commerce.Domain.Common;
 global using e_commerce.Domain.Events;
+global using e_commerce.Domain.Enums;
 global using FluentAssertions;
 global using Xunit;
-global using e_commerce.Domain.Enums;
-global using e_commerce.Domain.Models;
