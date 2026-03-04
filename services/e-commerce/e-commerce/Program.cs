c7f6caa91502cebe4c470fbacb8b02ef059e9fd0
Mukhammadaziz <juraev.mukhammadaziz@gmail.com>
2026-03-04 14:03:47 +0500

    wire up infrastructure in API layer

--- services/e-commerce/e-commerce/Program.cs
+++ services/e-commerce/e-commerce/Program.cs
@@ -1,11 +1,14 @@
+using e_commerce.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Register Infrastructure (DbContext, Repositories)
+builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
